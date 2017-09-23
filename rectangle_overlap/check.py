60bb091a9e9de394ac8d65e37b145847a26e0acb
Asterios Vounotrypidis <avounotr@gmail.com>
2017-09-24 02:07:12 +0300

    A finder for every overlap added

--- rectangle_overlap/check.py
+++ rectangle_overlap/check.py
@@ -9,6 +9,16 @@ def doOverlap(rectangle1, rectangle2):
 
     return True
 
-def getAllOverlaps(basicRectangle, rectangles):
+def getOverlapRectangles(basicRectangle, rectangles):
     overlaps = list(map(lambda rectangle: doOverlap(basicRectangle, rectangle), rectangles))
     return [index for index, overlap in enumerate(overlaps) if overlap]
+
+def findAllOverlaps(rectangles):
+    allOverlaps = []
+    for index, rectangle in enumerate(rectangles):
+        if ((index + 1) != len(rectangles)):
+            overlaps = [x + index + 1 for x in getOverlapRectangles(rectangle, rectangles[index + 1:])]
+            if(len(overlaps) > 0):
+                allOverlaps.extend([index] + overlaps)
+
+    return list(set(allOverlaps))
